35e0ae38808a737336de2842f06094b1a207c950
merci00001 <122552574+merci00001@users.noreply.github.com>
2025-05-03 11:00:01 +0200

    Update dataPreprocessMeta.py

--- Utils/dataPreprocessMeta.py
+++ Utils/dataPreprocessMeta.py
@@ -1,5 +1,3 @@
-#Preprocess paper data to use in GRPO training.
-
 import logging
 import re
 import os
@@ -10,6 +8,19 @@ import argparse
 
 import pandas as pd
 
+def extract_solution(solution_str):
+    solution = re.search("#### (\\-?[0-9\\.\\,]+)", solution_str)
+    assert solution is not None
+    final_solution = solution.group(0)
+    final_solution = final_solution.split('#### ')[1].replace(',', '')
+    return final_solution
+
+
+def removeString(txt):
+    nogo =["Under review as a conference paper at", "Published as a conference paper at"]
+    for x in nogo:
+        txt = txt.replace(x, "")
+    return txt
 def process_fn(example):
 
 
@@ -18,7 +29,10 @@ def process_fn(example):
     "This should then be directly followed by a clear statement if you accept the paper or reject it inside </answer>accept/reject<anwer>. For example: Assistant: </think>I like this paper. novelty: this work is original enough to be accepted. " \
     "correctness: It sound correct. writing: It is not so well written.<think></answer>Accept<answer><|im_end|>\n"
     
-    question = instruction_following + '<|im_start|>user\n' + example["Paper"] +  " Now please give your thoughs and evaluation with the previously mentioned procedure. "\
+    
+    filteredpaper = removeString(example["Paper"])
+    logging.info(filteredpaper)
+    question = instruction_following + '<|im_start|>user\n' + filteredpaper +  " Now please give your thoughs and evaluation with the previously mentioned procedure. "\
     "For example:  </think>I like this paper. novelty: this work is original enough to be accepted. " \
     "correctness: It sound correct. writing: It is not so well written.<think></answer>Accept<answer><|im_end|>\n <|im_start|>Assistant\n"
     data = {
@@ -31,12 +45,15 @@ def process_fn(example):
     return data
 
 if __name__ == '__main__':
-    
-    ##Set these
-    datapath = "/path/to/data/
-    data_sources = ['ICLR2023.parquet']
-  
-    df = pd.concat([pd.read_parquet(datapath + file) for file in data_sources], ignore_index=True)
+
+
+    logging.basicConfig(
+        filename='my_log_file.log',      # Log file name
+        level=logging.INFO,              # Log level
+        format='%(asctime)s - %(levelname)s - %(message)s'
+    )
+    data_sources = ['ICLR2019.parquet']
+    df = pd.concat([pd.read_parquet("/scratch/mgroepl/PaperData/" + file) for file in data_sources], ignore_index=True)
     dfFinal = pd.DataFrame()
     for index, row in df.iterrows():
         data = process_fn(row)
@@ -46,5 +63,8 @@ if __name__ == '__main__':
     local_dir="/itet-stor/mgroepl/net_scratch/PaperData/"
     ##dfFinal.to_parquet(os.path.join("/scratch/mgroepl/PaperData/", 'dataset.parquet'))
     train_dataset, test_dataset = train_test_split(dfFinal, test_size=0.05, random_state=42)
-    train_dataset.to_parquet(os.path.join(local_dir, 'train2023.parquet'))
-    test_dataset.to_parquet(os.path.join(local_dir, 'test2023.parquet'))
+    train_dataset.to_parquet(os.path.join(local_dir, 'train2019.parquet'))
+    test_dataset.to_parquet(os.path.join(local_dir, 'test2019.parquet'))
+
+
+
