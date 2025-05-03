d1c5990e50c7c8a3a9f9b797f789aa0ff8e394e5
merci00001 <122552574+merci00001@users.noreply.github.com>
2025-05-03 14:19:45 +0200

    Create evalPapers.py

---
+++ Utils/evalPapers.py
@@ -0,0 +1,128 @@
+from datasets import load_dataset
+from trl import GRPOConfig, GRPOTrainer
+import re
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import logging
+import torch
+
+def getScore(input,gT):
+    scores = []
+    gAccept = None
+    for x in input:
+        res = x[-80:].split("</answer>")
+
+        if len(res)==2:
+            answer = res[1]
+            reject = re.search(r"\b([1-5])\b", answer)
+            accept = re.search(r"\b(10|[6-9])\b", answer)
+            gAccept = re.search(r"accept", gT,  re.IGNORECASE)
+            gReject = re.search(r"reject", gT,  re.IGNORECASE)
+            if accept and gAccept:
+                scores.append(1)
+            elif reject and gReject:
+                scores.append(-1)
+            else:
+                scores.append(0)
+    return [scores, gAccept]
+logging.basicConfig(
+    filename='PaperEval3BRLFullPaperGen1CORRECTEVALSCRIPTOldModel.log',      # Log file name
+    level=logging.INFO,              # Log level
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+logging.info("starting")
+
+
+dataset = load_dataset("parquet", data_files="/itet-stor/mgroepl/net_scratch/PaperData/train2023.parquet")['train'] ##train2023  eval2023Summary
+
+
+
+model_name = "/srv/beegfs02/scratch/mgroepl_master_data/data/Qwen/Qwen/Qwen2.5-3BInstructFullPaperBiggerLrBetterDrop/checkpoint-7500"  #/itet-stor/mgroepl/net_scratch/Qwen/Qwen2.5-3B-Instruct-FT/checkpoint-507, /srv/beegfs02/scratch/mgroepl_master_data/data/Qwen/Qwen/Qwen2.5-3BInstructFullPaperBiggerLrCORRECT/checkpoint-8500, /srv/beegfs02/scratch/mgroepl_master_data/data/Qwen/Qwen/Qwen2.5-3BInstructSummary/checkpoint-6000
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    torch_dtype="auto",
+    device_map="auto"
+)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+correct = 0
+total = 0
+confusion = {"TP": 0,
+             "FP" : 0,
+             "TN": 0,
+             "FN":0}
+logging.info("starting")
+for x in dataset:
+    try:
+        y = x["ground_truth"]
+        total +=1
+        num_generations = 1
+        responses = []
+        print("starting")
+        prompt = lines = x["prompt"].split('<|im_end|>')
+        instruction_following =prompt[0].replace("<|im_start|>system\n","")
+        question = prompt[1].replace("<|im_start|>user","")
+
+        messages = [
+            {"role": "system", "content": instruction_following},
+            {"role": "user", "content": question}
+            ]
+        text = tokenizer.apply_chat_template(
+            messages,
+            tokenize=False,
+            add_generation_prompt=True
+        )
+        model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
+
+        u = 0
+        failed = 0
+        scores = []
+        while u < num_generations:
+            with torch.no_grad():
+                logging.info("generate")
+
+
+                generated_ids = model.generate(
+                    **model_inputs,
+                    max_new_tokens=1000
+                )
+                generated_ids = [
+                    output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+                ]
+
+                response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+                logging.info(response)
+                t = getScore([response],y)
+                score = t[0]
+                acc = t[1]
+                if len(score) == 1:
+                    scores.append(score[0])
+                    u+=1
+                else:
+                    failed +=1
+                if failed >=5:
+                    break
+        if failed >=5:
+            continue
+        logging.info(scores)
+
+        value = sum(scores)/len(scores)
+        if value > 0.5:
+            confusion["TP"] += 1
+            
+        elif value < -0.5:
+            correct +=1
+            confusion["TN"] +=1
+
+        elif value < 0.5 and value>=0 and acc:
+            confusion["FN"] +=1
+        else:
+            confusion["FP"] +=1
+        logging.info(confusion)
+    except RuntimeError as e:
+        if 'CUDA out of memory' in str(e):
+            print(f"[Warning] Skipping batch  due to CUDA OOM.")
+            torch.cuda.empty_cache()  # optional: clean up memory
+            continue
+        else:
+            raise 
+logging.info("end")
